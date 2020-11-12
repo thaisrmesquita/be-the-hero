e8d228f2bc319106e30431612a66a026157f367e
Thais RIbeiro <thaisribeiro.mesquita@gmail.com>
2020-11-12 18:34:44 -0300

    Build

---
+++ frontend/build/precache-manifest.1dc923cd482777a06349c511ac653267.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "049711316a48a959053221fbfec71f0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94e9343c381fb8e1cba3",
+    "url": "/static/css/main.2c2a065e.chunk.css"
+  },
+  {
+    "revision": "021baf7baea1f456c9c0",
+    "url": "/static/js/2.c4c075dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c4c075dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94e9343c381fb8e1cba3",
+    "url": "/static/js/main.33718b83.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "7eea718b48893e461f82049ec46c1e1c",
+    "url": "/static/media/logo.7eea718b.svg"
+  }
+]);
